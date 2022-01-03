104c0c22e979c6a377ace5d5649a9f7c33ecf160
DarkLobox <DarkLobox@DESKTOP-5IRK1J3>
2022-01-03 16:54:32 -0500

    Agregar archivos de proyecto.

---
+++ Gif/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace Gif
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.ocultarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pausarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.configuracionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.configuracionesToolStripMenuItem,
+            this.ocultarToolStripMenuItem,
+            this.pausarToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 114);
+            // 
+            // ocultarToolStripMenuItem
+            // 
+            this.ocultarToolStripMenuItem.Name = "ocultarToolStripMenuItem";
+            this.ocultarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ocultarToolStripMenuItem.Text = "Ocultar";
+            this.ocultarToolStripMenuItem.Click += new System.EventHandler(this.ocultarToolStripMenuItem_Click);
+            // 
+            // pausarToolStripMenuItem
+            // 
+            this.pausarToolStripMenuItem.Name = "pausarToolStripMenuItem";
+            this.pausarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.pausarToolStripMenuItem.Text = "Pausar";
+            this.pausarToolStripMenuItem.Click += new System.EventHandler(this.pausarToolStripMenuItem_Click);
+            // 
+            // configuracionesToolStripMenuItem
+            // 
+            this.configuracionesToolStripMenuItem.Name = "configuracionesToolStripMenuItem";
+            this.configuracionesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.configuracionesToolStripMenuItem.Text = "Configuraciones";
+            this.configuracionesToolStripMenuItem.Click += new System.EventHandler(this.configuracionesToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Gif";
+            this.notifyIcon1.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ocultarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pausarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem configuracionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ Gif/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Gif
+{
+    public partial class Form1 : Form
+    {
+        PictureBox imagen = new PictureBox();
+        static Graphics g;
+        int heightWindow = Screen.PrimaryScreen.WorkingArea.Height;
+        int widthWindow = Screen.PrimaryScreen.WorkingArea.Width;
+        static int frameWidth = 200;
+        static int frameHeight = 326;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            int localWidth = widthWindow - frameWidth;
+            int localHeight = heightWindow - frameHeight + 15;
+
+            ShowInTaskbar = false;
+            TopMost = true;
+            Width = frameWidth + 40;
+            Height = frameHeight + 40;
+            BackColor = Color.DarkGray;
+            TransparencyKey = Color.DarkGray;
+            StartPosition = FormStartPosition.Manual;
+            Location = new Point(localWidth, localHeight);
+
+            FormBorderStyle = FormBorderStyle.None;
+
+            imagen.Width = frameWidth;
+            imagen.Height = frameHeight;
+            imagen.SizeMode = PictureBoxSizeMode.Zoom;
+            var res = Image.FromFile(Application.StartupPath+@"\imagenes\EMT.gif");
+            ScaleImage(res, frameWidth, frameHeight);
+            imagen.Image = res;
+            this.Controls.Add(imagen);
+        }
+
+        static void ScaleImage(Image res, int maxWidth, int maxHeight)
+        {
+            var ratioX = (double)maxWidth / res.Width;
+            var ratioY = (double)maxHeight / res.Height;
+            var ratio = Math.Min(ratioX, ratioY);
+            var newWidth = (int)(res.Width * ratio);
+            var newHeight = (int)(res.Height * ratio);
+            Bitmap mapa = new Bitmap(newWidth, newHeight);
+            g = Graphics.FromImage(mapa);
+            g.DrawImage(res, 0, 0, newWidth, newHeight);
+            g.Dispose();
+        }
+
+        private void ocultarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+              
+            if (this.Visible)
+            {
+                this.Hide();
+                ocultarToolStripMenuItem.Text = "Mostrar";
+            }
+            else if (!this.Visible)
+            {
+                this.Show();
+                this.ShowInTaskbar = false;
+                ocultarToolStripMenuItem.Text = "Ocultar";
+            }
+            
+        }
+
+        private void configuracionesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pausarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (imagen.Enabled)
+            {
+                imagen.Enabled = false;
+                pausarToolStripMenuItem.Text = "Reproducir";
+            }
+            else if (!imagen.Enabled)
+            {
+                imagen.Enabled = true;
+                pausarToolStripMenuItem.Text = "Pausar";
+            }
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
